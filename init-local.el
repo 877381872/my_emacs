8a326028a9dea1dc36fe01a79c08fc3990fde0b6
Veneno <zongmeng1994@outlook.com>
2016-07-02 18:53:33 +0800

    update readme

--- init-local.el
+++ init-local.el
@@ -20,8 +20,7 @@
      (require 'tern-auto-complete)
      (tern-ac-setup)))
 ;;; js config
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'\\'" . js2-mode))
-
+(add-to-list 'auto-mode-alist '(".js[x]?\\'" . web-mode))
 (setq-default
  ;; js2-mode
  js2-basic-offset 2
@@ -36,5 +35,32 @@
  web-mode-enable-css-colorization t
  web-mode-enable-auto-pairing t
  web-mode-enable-block-face t)
+(require 'web-beautify) ;; Not necessary if using ELPA package
+(eval-after-load 'js2-mode
+  '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+(eval-after-load 'json-mode
+  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+(eval-after-load 'sgml-mode
+  '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+(eval-after-load 'css-mode
+  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+(eval-after-load 'js2-mode
+  '(add-hook 'js2-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
 
+(eval-after-load 'json-mode
+  '(add-hook 'json-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'sgml-mode
+  '(add-hook 'html-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+
+(eval-after-load 'css-mode
+  '(add-hook 'css-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
 (provide 'init-local)
