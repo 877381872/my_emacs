7b8edda66c9a9ee0e8782d0109eda2f195b24d1d
Veneno <zongmeng1994@outlook.com>
2016-07-08 12:00:58 +0800

    update the init file

--- init-local.el
+++ init-local.el
@@ -20,21 +20,26 @@
      (require 'tern-auto-complete)
      (tern-ac-setup)))
 ;;; js config
-(add-to-list 'auto-mode-alist '(".js[x]?\\'" . web-mode))
-(setq-default
- ;; js2-mode
- js2-basic-offset 2
- ;; web-mode
- css-indent-offset 2
- web-mode-markup-indent-offset 2
- web-mode-css-indent-offset 2
- web-mode-code-indent-offset 2
- web-mode-attr-indent-offset 4
- web-mode-enable-current-element-highlight t
- web-mode-enable-current-column-highlight t
- web-mode-enable-css-colorization t
- web-mode-enable-auto-pairing t
- web-mode-enable-block-face t)
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 (require 'web-beautify) ;; Not necessary if using ELPA package
 (eval-after-load 'js2-mode
   '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
