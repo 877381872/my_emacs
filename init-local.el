68971d075a44183fee6dfdfb3fa52f5aacfba6f7
Veneno <zongmeng1994@outlook.com>
2016-07-24 15:40:55 +0800

    update custom.el

--- init-local.el
+++ init-local.el
@@ -1,16 +1,9 @@
-;;; =========================== ;;; Comentary: ;;; ============================
-
-;;; =========== ;;; package --- Summary ;;; ============
-
 ;;; self-config
 (add-hook 'prog-mode-hook 'linum-mode)  ;显示行数
 (display-time-mode 1) ; 显示时间
 (setq display-time-24hr-format t) ; 24小时格式
 (setq display-time-day-and-date t) ; 显示日期
-(global-auto-complete-mode t)
-;;; neotree配置
-(add-to-list 'load-path "/directory/containing/neotree/")
-(require 'neotree)
+(global-auto-complete-mode t) ;;; globel auto-complete
 (global-set-key [f9] 'neotree-toggle)
 ;;; emacs config
 (autoload 'tern-mode "tern.el" nil t)
@@ -24,43 +17,13 @@
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js-mode-hook 'js2-minor-mode)
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
-(require 'react-snippets)
-(eval-after-load 'js2-mode
-  '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
-(eval-after-load 'json-mode
-  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
-(eval-after-load 'sgml-mode
-  '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
-(eval-after-load 'css-mode
-  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
-(eval-after-load 'js2-mode
-  '(add-hook 'js2-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
-
-(eval-after-load 'json-mode
-  '(add-hook 'json-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
-
-(eval-after-load 'sgml-mode
-  '(add-hook 'html-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
-
-(eval-after-load 'css-mode
-  '(add-hook 'css-mode-hook
-             (lambda ()
-               (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
-;;; ac-js2
-(add-hook 'js2-mode-hook 'ac-js2-mode)
-(add-hook 'js2--jsx-mode-hook 'ac-js2-mode)
-(setq ac-js2-evaluate-calls t)
-;;; go-auto-complete config
+;;; undo-tree
+(global-undo-tree-mode)
+;;; go-config
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 (ac-config-default)
-(provide 'init-local)
-;;; init-local.el ends here
+;;; 第三方插件
+(add-to-list 'load-path (concat dotfiles-dir "selfPlugins"))
+(provide 'custom)
+;;; custom.el ends here
